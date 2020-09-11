2622c0fa1097988fc66acd8dfed00b3d025a9f69
utoshkentov@gmail.com <Umrzoq8577840>
2020-09-11 17:20:14 +0500

    Updates

---
+++ precache-manifest.6bfcddb0d5f5a6dde8a7e5e937cdb3a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a241ac4d5324a77ecd5ca2925875984b",
+    "url": "/deployment/index.html"
+  },
+  {
+    "revision": "0a6f40524f8725b01fad",
+    "url": "/deployment/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "808fcb0c227609041b31",
+    "url": "/deployment/static/css/main.cab92878.chunk.css"
+  },
+  {
+    "revision": "0a6f40524f8725b01fad",
+    "url": "/deployment/static/js/2.05ad36c0.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/deployment/static/js/2.05ad36c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808fcb0c227609041b31",
+    "url": "/deployment/static/js/main.6a60f56d.chunk.js"
+  },
+  {
+    "revision": "6a4ec53dbc10064852aa",
+    "url": "/deployment/static/js/runtime-main.e763854f.js"
+  }
+]);
